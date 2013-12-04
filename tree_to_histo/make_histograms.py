e053f26be31a20b743a0193819324e4cd75cffdf
liis <polaarrebane@gmail.com>
2013-12-04 15:06:40 +0100

    clean up

--- tree_to_histo/make_histograms.py
+++ tree_to_histo/make_histograms.py
@@ -1,7 +1,7 @@
 import ROOT
 from array import array
 from tree_variables import var_list, var_type
-from histlib import fill_hist_ratio
+from histlib import fill_hist_ratio, log_binning
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -49,28 +49,16 @@ h_fake_eta = ROOT.TH1F("nfake_eta","nrFake vs eta", neta, mineta, maxeta)
 h_sim_eta = ROOT.TH1F("nsim_eta", "nrSim vs eta", neta, mineta, maxeta)
 h_sim_to_reco_match_eta = ROOT.TH1F("nsimtoreco_eta", "nr sim to reco matched vs eta", neta,mineta,maxeta)
 
-h_fakerate_eta = ROOT.TH1F("fake_rate_eta","Fake rate", neta, mineta, maxeta)
-h_eff_eta = ROOT.TH1F("eff_eta","Efficiency", neta, mineta, maxeta)
-
 npt = 50
 minpt = 0.1
 maxpt = 200
 
-logxmin = ROOT.TMath.log10(minpt); #get logarithmic bins for pt histograms
-print "logxmin = " + str(logxmin)
-logxmax = ROOT.TMath.log10(maxpt);
-binwidth = (logxmax-logxmin)/npt;
-xbins = []
-xbins.append(minpt);
-for i in range(1,npt+1):
-    xbins.append(minpt + ROOT.TMath.Power(10,logxmin + i*binwidth))
+xbins = log_binning(npt,minpt,maxpt) #log binning for pt histograms
 
-#h_reco_pt_barrel = ROOT.TH1F("nreco_pt_barrel","nrReco vs pt barrel", npt, minpt, maxpt)
 h_reco_pt_barrel = ROOT.TH1F("nreco_pt_barrel","nrReco vs pt barrel", npt, array('d',xbins) )
 h_reco_pt_endcap = ROOT.TH1F("nreco_pt_endcap","nrReco vs pt endcap", npt, array('d',xbins) )
 h_reco_pt_trans = ROOT.TH1F("nreco_pt_trans","nrReco vs pt trans", npt, array('d',xbins) )
 
-#h_fake_pt_barrel = ROOT.TH1F("nfake_pt_barrel","nrFake vs pt barrel", npt, minpt, maxpt)
 h_fake_pt_barrel = ROOT.TH1F("nfake_pt_barrel","nrFake vs pt barrel", npt, array('d', xbins) )
 h_fake_pt_endcap = ROOT.TH1F("nfake_pt_endcap","nrFake vs pt endcap", npt, array('d', xbins) )
 h_fake_pt_trans = ROOT.TH1F("nfake_pt_trans","nrFake vs pt trans", npt, array('d',xbins) )
@@ -82,15 +70,6 @@ h_sim_pt_trans = ROOT.TH1F("sim_pt_trans", "nrSim vs pt", npt, array('d', xbins)
 h_sim_to_reco_match_pt_barrel = ROOT.TH1F("nsimtoreco_pt_barrel", "nr sim to reco matched vs pt", npt,array('d', xbins))
 h_sim_to_reco_match_pt_endcap = ROOT.TH1F("nsimtoreco_pt_endcap", "nr sim to reco matched vs pt", npt, array('d',xbins))
 h_sim_to_reco_match_pt_trans = ROOT.TH1F("nsimtoreco_pt_trans", "nr sim to reco matched vs pt", npt, array('d', xbins))
-
-#h_fakerate_pt_barrel = ROOT.TH1F("fake_rate_pt_barrel","Fake rate barrel", npt, minpt, maxpt)
-h_fakerate_pt_barrel = ROOT.TH1F("fake_rate_pt_barrel","Fake rate barrel", npt, array('d',xbins))
-h_fakerate_pt_endcap = ROOT.TH1F("fake_rate_pt_endcap","Fake rate endcap", npt, array('d',xbins))
-h_fakerate_pt_trans = ROOT.TH1F("fake_rate_pt_trans","Fake rate trans", npt, array('d',xbins))
-
-h_eff_pt_barrel = ROOT.TH1F("eff_pt_barrel","Efficiency barrel", npt, array('d',xbins))
-h_eff_pt_endcap = ROOT.TH1F("eff_pt_endcap","Efficiency endcap", npt, array('d',xbins))
-h_eff_pt_trans = ROOT.TH1F("eff_pt_trans","Efficiency trans", npt, array('d',xbins))
 
 # Event loop
 for i in range(nEvt):
@@ -137,47 +116,18 @@ for i in range(nEvt):
         elif abs(vt['gen_eta'][it_p]) < 2.5:
             h_sim_to_reco_match_pt_endcap.Fill(vt['gen_matched_pt'][it_p])
 
-# efficiency and fake rate wrt eta
-for i in range(1,neta+1): 
-    if not h_reco_eta.GetBinContent(i) == 0:
-        h_fakerate_eta.SetBinContent(i, h_fake_eta.GetBinContent(i)/h_reco_eta.GetBinContent(i) )
-    else:
-        h_fakerate_eta.SetBinContent(i,0)
-        
-    if not h_sim_eta.GetBinContent(i) == 0:
-        h_eff_eta.SetBinContent(i, h_sim_to_reco_match_eta.GetBinContent(i)/h_sim_eta.GetBinContent(i) )
-    else:
-        h_sim_eta.SetBinContent(i,0)
+#efficiency and fake rate wrt pt
+h_fakerate_eta = fill_hist_ratio(h_fake_eta, h_reco_eta,"fake_rate_eta")
+h_eff_eta = fill_hist_ratio(h_sim_to_reco_match_eta, h_sim_eta, "eff_eta")    
 
 # efficiency and fake rate wrt pt
-for i in range(1,npt+1):
-    if not h_reco_pt_barrel.GetBinContent(i) == 0:
-        h_fakerate_pt_barrel.SetBinContent(i, h_fake_pt_barrel.GetBinContent(i)/h_reco_pt_barrel.GetBinContent(i) )
-    else:
-        h_fakerate_pt_barrel.SetBinContent(i,0)
-    if not h_reco_pt_endcap.GetBinContent(i) == 0:
-        h_fakerate_pt_endcap.SetBinContent(i, h_fake_pt_endcap.GetBinContent(i)/h_reco_pt_endcap.GetBinContent(i) )
-    else:
-        h_fakerate_pt_endcap.SetBinContent(i,0)
+h_eff_pt_barrel = fill_hist_ratio(h_sim_to_reco_match_pt_barrel, h_sim_pt_barrel, "eff_pt_barrel", binning="log")
+h_eff_pt_trans = fill_hist_ratio(h_sim_to_reco_match_pt_trans, h_sim_pt_trans, "eff_pt_trans", binning="log")
+h_eff_pt_endcap = fill_hist_ratio(h_sim_to_reco_match_pt_endcap, h_sim_pt_endcap, "eff_pt_endcap", binning="log")
 
-    if not h_reco_pt_trans.GetBinContent(i) ==0:
-        h_fakerate_pt_trans.SetBinContent(i, h_fake_pt_trans.GetBinContent(i)/h_reco_pt_trans.GetBinContent(i) )
-    else:
-        h_fakerate_pt_trans.SetBinContent(i,0)
-        
-    if not h_sim_pt_barrel.GetBinContent(i) == 0:
-        h_eff_pt_barrel.SetBinContent(i, h_sim_to_reco_match_pt_barrel.GetBinContent(i)/h_sim_pt_barrel.GetBinContent(i) )
-    else:
-        h_eff_pt_barrel.SetBinContent(i,0)
-    if not h_sim_pt_endcap.GetBinContent(i) == 0:
-        h_eff_pt_endcap.SetBinContent(i, h_sim_to_reco_match_pt_endcap.GetBinContent(i)/h_sim_pt_endcap.GetBinContent(i) )
-    else:
-        h_eff_pt_endcap.SetBinContent(i,0)
-    if not h_sim_pt_trans.GetBinContent(i) == 0:
-        h_eff_pt_trans.SetBinContent(i, h_sim_to_reco_match_pt_trans.GetBinContent(i)/h_sim_pt_trans.GetBinContent(i) )
-    else:
-        h_eff_pt_trans.SetBinContent(i,0)
-h_eff_pt_trans = fill_hist_ratio(h_sim_to_reco_match_pt_trans, h_sim_pt_trans)
+h_fakerate_pt_barrel = fill_hist_ratio(h_fake_pt_barrel, h_reco_pt_barrel, "fake_rate_pt_barrel", binning = "log")
+h_fakerate_pt_trans = fill_hist_ratio(h_fake_pt_trans, h_reco_pt_trans, "fake_rate_pt_trans", binning = "log")
+h_fakerate_pt_endcap = fill_hist_ratio(h_fake_pt_endcap, h_reco_pt_endcap, "fake_rate_pt_endcap", binning = "log")
 
 outfile = "histograms/trackValHistograms" + args.ptmode  + ".root"    
 o = ROOT.TFile(outfile,"recreate")

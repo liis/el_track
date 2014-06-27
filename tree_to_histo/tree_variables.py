15a8af2eed50f18c114953b1319af94f4eb38e9f
liis <polaarrebane@gmail.com>
2014-06-27 16:32:23 +0200

    efficiency histograms with 71X

--- tree_to_histo/tree_variables.py
+++ tree_to_histo/tree_variables.py
@@ -3,18 +3,20 @@ import numpy as np
 maxpart = 100
 maxhit = 30
 
-int_list = ["np_reco", "np_gen", "np_fake", "np_gen_toReco", "gen_matchedSeedOkCharge"]
-int_array_list = ["gen_nrMatchedSeedHits", "gen_nrUniqueSimHits", "gen_reco_matched", "is_ecalDrivenSeed", "is_trackerDrivenSeed", "gen_matchedSeedOkCharge"] 
-
+int_list = ["np_reco", "np_gen", "np_fake", "np_gen_toReco"]
+ 
 int_2D_array_list = ["gen_hit_subdetector", "gen_hit_layer"]
 float_2D_array_list = ["gen_hit_pt"]
 
 double_list = []
 double_array_list_hits = []
 double_array_list_res_pull = ["pt_pull", "qoverp_pull", "theta_pull", "phi_pull", "d0_pull", "z0_pull", "gen_matched_rec_pt", "gen_matched_cotth", "gen_matched_rec_cotth", "gen_matched_rec_qoverp", "gen_matched_qoverp", "gen_matched_z0", "gen_matched_rec_z0", "gen_matched_rec_phi", "gen_matched_d0", "gen_matched_rec_d0"] #, "gen_matched_theta", "gen_matched_rec_theta"]
-double_array_list = ["reco_pt", "reco_eta", "reco_phi", "gen_pt", "gen_eta", "gen_phi", "fake_pt", "fake_eta", "fake_phi", "gen_matched_eta", "gen_matched_phi", "gen_matched_pt", "gen_dxy", "gen_dz", "gen_bremFraction"] + double_array_list_hits + double_array_list_res_pull
 
-var_list = int_list + int_array_list + double_list + double_array_list + int_2D_array_list + float_2D_array_list
+eff_double_array_list = ["reco_pt", "reco_eta", "gen_pt", "gen_eta", "fake_pt", "fake_eta", "gen_matched_eta", "gen_matched_pt", "gen_matchedSeedQuality"]
+eff_int_array_list = ["gen_reco_matched", "is_ecalDrivenSeed", "is_trackerDrivenSeed", "gen_nrMatchedSeedHits", "gen_matchedSeedOkCharge"]
+
+double_array_list = ["reco_phi", "gen_phi", "fake_phi", "gen_matched_phi", "gen_dxy", "gen_dz", "gen_bremFraction"] + double_array_list_hits + double_array_list_res_pull + eff_double_array_list
+int_array_list = ["gen_nrUniqueSimHits"] + eff_int_array_list 
 
 def var_type(var_name):
     """
@@ -28,4 +30,6 @@ def var_type(var_name):
     if var_name in int_2D_array_list: return np.array( np.zeros( (maxpart, maxhit) ), dtype=np.int32) 
     if var_name in float_2D_array_list: return np.array( np.zeros( (maxpart, maxhit) ), dtype=np.float32 ) 
 
-#if var_name in int_2D_array_list return array('i', [0]*maxpart) #np.array(np.zeros(maxpart,maxhit), dtype=int) )
+
+eff_list = int_list + eff_double_array_list + eff_int_array_list 
+var_list = int_list + int_array_list + double_list + double_array_list + int_2D_array_list + float_2D_array_list
